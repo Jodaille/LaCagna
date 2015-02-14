b88c1927cbe665121f28480b9c324d98e4235559
Jodaille <jody.noury@gmail.com>
2015-02-14 13:26:53 +0100

    add zfcuser translator files + default fr

--- module/LaCagnaUser/Module.php
+++ module/LaCagnaUser/Module.php
@@ -8,6 +8,22 @@ class Module
         return include __DIR__ . '/config/module.config.php';
     }
 
+    public function onBootstrap(\Zend\Mvc\MvcEvent $e)
+    {
+        // Adding translators
+        $translator = $e->getApplication()->getServiceManager()->get('translator');
+
+        // ZfcUser translator
+        $translator->addTranslationFile(
+        'gettext',
+        'vendor/zf-commons/zfc-user/src/ZfcUser/language/fr_FR.mo',
+        'default',
+        'fr_FR'
+        );
+
+        \Zend\Validator\AbstractValidator::setDefaultTranslator(new \Zend\Mvc\I18n\Translator($translator));
+    }
+
     public function getAutoloaderConfig()
     {
         return array(
