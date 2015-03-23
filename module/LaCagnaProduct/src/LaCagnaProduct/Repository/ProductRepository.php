dd05004697e0c1d2d128fb6f83d29ff6315ca9f7
Jodaille <jody.noury@gmail.com>
2015-03-23 23:04:20 +0100

    navigation by category

--- module/LaCagnaProduct/src/LaCagnaProduct/Repository/ProductRepository.php
+++ module/LaCagnaProduct/src/LaCagnaProduct/Repository/ProductRepository.php
@@ -28,4 +28,64 @@ class ProductRepository extends EntityRepository
     $query = $qb->getQuery();
     return $query->getResult();
   }
+  public function byCategoryCode($code, $state = 'disponible')
+  {
+
+    $category = $this->getEntityManager()
+                    ->getRepository('LaCagnaProduct\Entity\Category')
+                    ->findOneByCode($code);
+
+
+    $left           = $category->GetLeftValue();
+    $right          = $category->GetRightValue();
+    $qb = $this->getEntityManager()->createQueryBuilder();
+    $qb->select('p,c')
+    ->from('LaCagnaProduct\Entity\Product', 'p')
+    ->leftJoin('p.categories', 'c')
+    ->where('c.lft >= :left')
+    ->andWhere('c.rgt <= :right')
+    //->andWhere('p.state = :state')
+    ->setParameters(
+        array(':left' => $left,
+              ':right' => $right,
+              //':state' => $state,
+              )
+        );
+
+    $qb->orderBy('p.title', 'ASC');
+    $query = $qb->getQuery();
+    //var_dump($query->getSql(),$left,$right, $code);
+
+    return $query->getResult();
+  }
+
+  public function byCategorySlug($slug, $state = 'disponible')
+  {
+
+    $category = $this->getEntityManager()
+                    ->getRepository('LaCagnaProduct\Entity\Category')
+                    ->findOneBySlug($slug);
+    $left =     $category->GetLeftValue();
+    $right =     $category->GetRightValue();
+    $qb = $this->getEntityManager()->createQueryBuilder();
+    $qb->select('p,c')
+    ->from('LaCagnaProduct\Entity\Product', 'p')
+    ->leftJoin('p.categories', 'c')
+    ->where('c.lft >= :left')
+    ->andWhere('c.rgt <= :right')
+    ->andWhere('p.state = :state')
+    ->andWhere('c.slug = :slug')
+
+    ->setParameters(
+        array(':left' => $left,
+              ':right' => $right,
+              ':state' => $state,
+              ':slug' => $slug,
+
+              )
+        );
+    $qb->orderBy('p.title', 'ASC');
+    $query = $qb->getQuery();
+    return $query->getResult();
+  }
 }
