d6204a19eba8bece5f5d37d951b24b035625ce44
Jodaille <j_noury@MAC0004.local>
2016-02-26 16:22:29 +0100

    cleanup code

--- public/js/adviseme.js
+++ public/js/adviseme.js
@@ -7,14 +7,7 @@ $(document).ready(function() {
           $.ajax({
               url: '/getadvise',
               type: 'GET',
-              success: function(result){
-
-                  if(result.products)
-                  {
-                      animateMe(result);
-                  }
-
-              },
+              success: animateMe,
               error: function(data) {
                   alert('woops!');
               }
@@ -25,60 +18,63 @@ $(document).ready(function() {
 
 function animateMe(results) {
   products = results.products;
-  var myProgress = document.getElementById("myProgress");
-  var elem = document.getElementById("myBar");
-  var width = 0;
 
-  var $nbElt = 0;
-  $nbElt = Object.keys(products).length;
-  myProgress.width = '100%';
-
-  minimum = 0;
-  var randomnumber = Math.floor(Math.random() * ($nbElt - minimum + 1)) + minimum;
   if(products)
   {
 
-      var pos = 0;
+    findProduct(products);
 
-      var id = setInterval(frame, 100);
-      function frame() {
-          $nbElt = Object.keys(products).length;
-
-          var product = products[pos];
-
-
-        if (pos == $nbElt) {
-          pos = 0;
-        } else {
-
-          if(products)
-          {
-              var myImg = document.getElementById("myProduct");
-              if(product.media_slug)
-              {
-                  var slug = product.media_slug;
-                  var imgSrc = slug.replace('/img/','/img/150/');
-                  myImg.src = imgSrc;
-              }
-              else
-              {
-                  myImg.src = '';
-              }
-              elem.style.width = pos + '%';
-          }
-          pos++;
-        }
-        if( randomnumber == pos)
-        {
-            clearInterval(id);
-            mybutton = document.getElementById("getAdvise");
-            mybutton.innerHTML = "Found: " + product.product_title;
-            elem.style.width = 100 + '%';
-        }
-      }
   }
   else
   {
       console.log('no products ');
   }
+}
+
+function findProduct(products) {
+    var myProgress = document.getElementById("myProgress");
+    var width      = 0;
+    var pos        = 0;
+    var minimum    = 0;
+
+    $nbElt           = Object.keys(products).length;
+    myProgress.width = '100%';
+
+    var randomnumber = Math.floor(Math.random() * ($nbElt - minimum + 1)) + minimum;
+
+    var id = setInterval(function() {
+        var product = products[pos];
+        var elem    = document.getElementById("myBar");
+        $nbElt      = Object.keys(products).length;
+
+      if (pos == $nbElt) {
+
+        pos = 0;
+      } else {
+
+        if(products) {
+
+            var myImg = document.getElementById("myProduct");
+            if(product.media_slug)
+            {
+                var slug = product.media_slug;
+                var imgSrc = slug.replace('/img/','/img/150/');
+                myImg.src = imgSrc;
+            }
+            else
+            {
+                myImg.src = '';
+            }
+            elem.style.width = pos + '%';
+        }
+        pos++;
+      }
+      if( randomnumber == pos)
+      {
+          clearInterval(id);
+          mybutton = document.getElementById("getAdvise");
+          mybutton.innerHTML = "Found: " + product.product_title;
+          elem.style.width = 100 + '%';
+      }
+    }, 100);
 }
